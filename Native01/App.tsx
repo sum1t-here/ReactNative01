e113dbdace880e68da8f2b12926a772950a1ca6a
sumit <mazumdarsumit3@gmail.com>
2024-02-07 18:22:46 +0530

    Add: hello world in react native

---
+++ Native01/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
