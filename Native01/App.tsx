a3584807441c8b15ff640951ee9a94a900656f5a
sumit <mazumdarsumit3@gmail.com>
2024-02-07 23:34:35 +0530

    Add: Dark mode

--- Native01/App.tsx
+++ Native01/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {Text, View, SafeAreaView} from 'react-native';
+import {
+  Text,
+  View,
+  // SafeAreaView,
+  useColorScheme,
+  StyleSheet,
+} from 'react-native';
 
-function App() {
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-    </SafeAreaView>
+    // <SafeAreaView>
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        Hello World
+      </Text>
+    </View>
+    // </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    // Left to right
+    justifyContent: 'center',
+    // top to bottom
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
 
 export default App;
